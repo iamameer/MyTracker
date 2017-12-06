3576b560a3c08bbc83179a84106bf7279d1f7443
Ameer Sorne <ameersorne@gmail.com>
2017-12-06 22:15:50 +0800

    (fixed)dded base-Activities:
    detail_activity
    done_activity
    history_activity
    setGoal_activity
    track_activity

---
+++ app/src/main/java/mdpcw2/mytracker/setGoal_activity.java
@@ -0,0 +1,13 @@
+package mdpcw2.mytracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class setGoal_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_goal_activity);
+    }
+}
