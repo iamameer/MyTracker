a4f834b2a92701e357ad240e5a6301930d066477
Ameer Sorne <ameersorne@gmail.com>
2017-12-10 22:19:00 +0800

    added methods for imgStart n imgHistory -prob: colorBox not working

--- app/src/main/java/mdpcw2/mytracker/MainActivity.java
+++ app/src/main/java/mdpcw2/mytracker/MainActivity.java
@@ -1,13 +1,93 @@
 package mdpcw2.mytracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Global Variables
+    private ImageView imgMainStart, imgMainHistory;
+
+    //Init
+    public void init(){
+        imgMainStart = findViewById(R.id.imgMainStart);
+        imgMainHistory = findViewById(R.id.imgMainHistory);
+    }
+
+    //setting up methods
+    public void setEvents(){
+        imgMainStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),track_activity.class);
+                startActivity(intent);
+            }
+        });
+
+        imgMainHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),history_activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        setEvents();
     }
+
+    //Activity Lifecycle onStart()
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("MyTracker","=MainActivity onStart()");
+    }
+
+    //Activity Lifecycle onRestart()
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("MyTracker","=MainActivity onRestart()");
+    }
+
+    //Activity Lifecycle onResume()
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("MyTracker","=MainActivity onResume()");
+    }
+
+    //Activity Lifecycle onPause()
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("MyTracker","=MainActivity onPause()");
+    }
+
+    //Activity Lifecycle onStop()
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("MyTracker","=MainActivity onStop()");
+    }
+
+    //Activity Lifecycle onDestroy()
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("MyTracker","=MainActivity onDestroy()");
+    }
+
+
 }
