36d162ab37768376dae8c4c8e705e81b645fe7dc
Ameer Sorne <ameersorne@gmail.com>
2017-12-11 15:07:35 +0800

    GPS //TrackerService actually working -BUT, something wrong with broadcast :c

--- app/src/main/java/mdpcw2/mytracker/non_activity/TrackerService.java
+++ app/src/main/java/mdpcw2/mytracker/non_activity/TrackerService.java
@@ -12,6 +12,7 @@ import android.os.IBinder;
 import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 public class TrackerService extends Service {
 
@@ -34,32 +35,38 @@ public class TrackerService extends Service {
     //https://www.youtube.com/watch?v=lvcGh2ZgHeA
     @SuppressLint("MissingPermission")
     private void startTracker(){
+        Log.d("MyTracker","@@MyTrackerService in startTracker");
         listener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
+                Log.d("MyTracker","@@MyTrackerService onLocationChanged");
                 Intent intent = new Intent("location_update");
-                intent.putExtra("coordinates",location.getLongitude()+" "+location.getLatitude());
+                intent.putExtra("coordinates",location.getLongitude()+"//"+location.getLatitude());
                 sendBroadcast(intent);
+                Toast.makeText(getApplicationContext(),"coordinates: "+location.getLongitude()+"//"+location.getLatitude(),Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onStatusChanged(String s, int i, Bundle bundle) {
-
+                Log.d("MyTracker","@@MyTrackerService onStatusChanged");
             }
 
             @Override
             public void onProviderEnabled(String s) {
-
+                Log.d("MyTracker","@@MyTrackerService onProviderEnabled");
             }
 
             @Override
             public void onProviderDisabled(String s) {
+                Log.d("MyTracker","@@MyTrackerService onProviderDisabled");
                 Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
             }
         };
+        Log.d("MyTracker","@@MyTrackerService getSystemService");
         locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        Log.d("MyTracker","@@MyTrackerService requestLocationUpdates");
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,500,0,listener);
     }
 
